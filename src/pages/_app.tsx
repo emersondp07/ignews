53fda3a3b63a997ed9796f8eea344d28a150a970
Emerson Dantas <emerson.dantaspereira@hotmail.com>
2023-08-29 22:08:31 -0300

    created connection with faunadb and created SignIn and SignOut with Github

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,16 @@
+import { SessionProvider as NextAuthProvider } from "next-auth/react";
 import { AppProps } from "next/app";
-
 import { Header } from "../components/Header";
+
 import "../styles/global.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Header />
-      <Component {...pageProps} />;
+      <NextAuthProvider session={pageProps.session}>
+        <Header />
+        <Component {...pageProps} />;
+      </NextAuthProvider>
     </>
   );
 }
